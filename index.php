3dfb52f0534d87e1c06710e67f268692d881127a
Cindy Leschaud <cindy.leschaud@gmail.com>
2020-10-05 15:11:34 +0200

    add readme

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 <?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$math = new src\Math();
+
+$numbers = [
+    5       => 1,
+    17      => 3,
+    25      => 2,
+    1223243 => 2,
+    251     => 1,
+    9       => 2,
+    529     => 4,
+    20      => 1,
+    32      => 0,
+    1041    => 5
+];
+
+echo '<pre>';
+print_r($math->binGap( 1223243 ));
+echo '</pre>';
+exit;
--- src/Math.php
+++ src/Math.php
@@ -258,4 +258,41 @@ class Math
         return $min;
     }
 
+    public function wall($wall,$m,$n)
+    {
+        // initializing output variables
+        $num_m = 0;
+        $num_n = 0;
+        $min_empty = $wall;
+
+        // p and q are no of shelves of length m and n
+        // rem is the empty space
+        $p   = 0;
+        $q   = 0;
+        $rem = 0;
+
+        while ($wall >= $n)
+        {
+            // place as many shelves of length m
+            // in the remaining part
+            $p = $wall / $m;
+            $rem = $wall % $m;
+
+            // update output variablse if curr
+            // min_empty <= overall empty
+            if ($rem <= $min_empty)
+            {
+                $num_m = $p;
+                $num_n = $q;
+                $min_empty = $rem;
+            }
+
+            // place one more shelf of length n
+            $q += 1;
+            $wall = $wall - $n;
+        }
+
+        echo $num_m , " ", $num_n , " ",
+        $min_empty ,"\n";
+    }
 }
